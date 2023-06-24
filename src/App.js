d545a5f54f5e0708141b316ea0d4e579b1f16feb
Sebastian Ruiz | Web Developer <121634416+SebastianDev017@users.noreply.github.com>
2023-06-24 07:59:37 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ await fetch("https://api.openai.com/v1/chat/completions",{
                 })
               }
             </MessageList >
-            <MessageInput placeholder='Mensaje' onSend={handleSend} />
+            <MessageInput placeholder='Mensaje' onSend={handleSend} style={{ position: "fixed", bottom: "20px"}}/>
           </ChatContainer>
         </MainContainer>
       </div>
