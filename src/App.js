2d5e56e66eab79998cb19da71bf9679612fad0f8
Sebastian Ruiz | Web Developer <121634416+SebastianDev017@users.noreply.github.com>
2023-06-25 21:15:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -94,15 +94,15 @@ await fetch("https://api.openai.com/v1/chat/completions",{
             <MessageList
             scrollBehavior='smooth'
             typingIndicator={typing ? <TypingIndicator content="SebasAI esta escribiendo..." /> : null}
-            
+            style={{paddingBottom:"80px"}}
             >
               {
                 messages.map((message,i) =>{
-                  return <Message key={i} model={message} style={{ width: "100%", height: "auto", padding: "0px",paddingBottom:"50px"}}/>
+                  return <Message key={i} model={message} style={{ width: "100%", height: "auto", padding: "0px",}}/>
                 })
               }
             </MessageList >
-            <MessageInput placeholder='Mensaje' onSend={handleSend} style={{ width:"72%", position: "fixed", bottom: "10px"}}/>
+            <MessageInput placeholder='Mensaje' onSend={handleSend} style={{ width:"70%", position: "fixed", bottom: "10px"}}/>
           </ChatContainer>
         </MainContainer>
       </div>
